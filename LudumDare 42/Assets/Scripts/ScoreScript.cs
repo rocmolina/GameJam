bbb2187c521450e8e5a330eecc980e711d89ab61
rocmolina <36119586+rocmolina@users.noreply.github.com>
2018-08-13 15:10:20 -0600

    Script Score

--- LudumDare 42/Assets/Scripts/ScoreScript.cs
+++ LudumDare 42/Assets/Scripts/ScoreScript.cs
@@ -59,7 +59,7 @@ public class ScoreScript : MonoBehaviour {
         }
 
 
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.1f);s
         StartCoroutine("Score");      
     }
 }
